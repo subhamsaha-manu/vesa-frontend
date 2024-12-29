d1b19f4de88b2119513ad92e8295cc8e145c66ca
Subham Saha <subhamsaha90@gmail.com>
2024-12-29 09:21:08 +0530

    WIP

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -14,6 +14,29 @@ export const system = createSystem(defaultConfig, {
         xl: { value: '80em' },
         '2xl': { value: '96em' },
       },
+      colors: {
+        brand: {
+          50: { value: '#e6f2ff' },
+          100: { value: '#e6f2ff' },
+          200: { value: '#bfdeff' },
+          300: { value: '#99caff' },
+          // ...
+          950: { value: '#001a33' },
+        },
+      },
+    },
+    semanticTokens: {
+      colors: {
+        brand: {
+          solid: { value: '{colors.brand.500}' },
+          contrast: { value: '{colors.brand.100}' },
+          fg: { value: '{colors.brand.700}' },
+          muted: { value: '{colors.brand.100}' },
+          subtle: { value: '{colors.brand.200}' },
+          emphasized: { value: '{colors.brand.300}' },
+          focusRing: { value: '{colors.brand.500}' },
+        },
+      },
     },
   },
 })
